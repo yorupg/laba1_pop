3fe3fd0c5409bbc92684988649509b90e45a2ce6
anna grezina <grezina.a18@gmail.com>
2026-09-12 23:00:57 +0300

    added base funck

--- src/main.py
+++ src/main.py
@@ -1,20 +1,16 @@
+import typer
+
 from src.power import power_function
 from src.constants import SAMPLE_CONSTANT
 
+app = typer.Typer()
 
-def main() -> None:
-    """
-    Обязательнная составляющая программ, которые сдаются. Является точкой входа в приложение
-    :return: Данная функция ничего не возвращает
-    """
 
-    target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
-
-    result = power_function(target=target, power=degree)
-
-    print(result)
-
-    print(SAMPLE_CONSTANT)
+@app.command()
+def main(name: str = typer.Argument(..., help="QWEKQWEOPQWKE")):
+    power_function(1, 2)
+    print(f'Hello {name}')
+    
 
 if __name__ == "__main__":
-    main()
+    app()
