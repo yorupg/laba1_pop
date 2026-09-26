775ea181fca57715d906cd897adcb2f0f7cc1be8
yorupg <grezina.a18@gmail.com>
2026-09-26 10:34:48 +0300

    __main__.py

--- src/toolkit/__main__.py
+++ src/toolkit/__main__.py
@@ -2,7 +2,15 @@ import typer
 
 from toolkit.calculator import calculator
 from toolkit.converter import converter
-from toolkit.errors import INPUT_ERROR, NO_NUMBERS, NO_SIGNS
+from toolkit.errors import (
+    BELOW_ZERO,
+    INCORRECT_DATA,
+    INPUT_ERROR,
+    NO_NUMBERS,
+    NO_SIGNS,
+    WRONG_CATEGORY,
+    WRONG_VALUE,
+)
 
 app = typer.Typer()
 
@@ -32,10 +40,10 @@ def convert(
     to: str = typer.Option(..., "--to", help="введите целевую единицу")):
     result = converter([value, from_, to])
     errors = {
-        "неверное числовое значение",
-        "ниже абсолютного нуля",
-        "данный тип не поддерживается",
-        "ошибка, введено недостаточно данных",
+        WRONG_VALUE,
+        BELOW_ZERO ,
+        WRONG_CATEGORY,
+        INCORRECT_DATA
     }
     if result in errors:
         typer.echo(result, err=True)
